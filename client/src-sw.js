b317947cb5cc1494d820f35e8cdcaa71531c5944
Cody Cooper <cody_codster@live.com>
2023-03-06 06:38:07 -0600

    Updated service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,7 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache); 
 
 // TODO: Implement asset caching
-registerRoute(({ request }) => ['style', 'script', 'worker', 'images'].includes(request.destination),
+registerRoute(({ request }) => ['style', 'script', 'worker', 'image', 'manifest', 'document'].includes(request.destination),
 new StaleWhileRevalidate({ //Grab from cache but also check network for updates
   cacheName: 'asset-cache',
   plugins: [
